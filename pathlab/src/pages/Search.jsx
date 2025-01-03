4be0729d6f91498af1fbd5a9115b6901c34a43c3
Parichay Didwania <parichaydidwania@gmail.com>
2025-01-03 06:13:15 -0500

    feat: lexographical search

--- pathlab/src/pages/Search.jsx
+++ pathlab/src/pages/Search.jsx
@@ -1,48 +1,59 @@
 import { useSearchParams } from "react-router-dom";
 import "./Search.css";
-import { useId } from "react";
+import { useEffect, useId, useState} from "react";
 import Card from "../components/Card";
 import SearchBar from "../components/SearchBar";
+import CONSTANTS from "../helpers/constants";
 
 function Search({ className, products, cart, setCart }) {
     const [searchParams] = useSearchParams();
     const id = useId();
+    const [closestProducts, setClosestProducts] = useState([]);
+    const [showlimit, setShowLimit] = useState(CONSTANTS.PAGINATION_LIMIT);
 
-    function getIdArray() {
+    useEffect(() => {
         const title = searchParams.get("title")?.toLowerCase();
         const productIds = [];
         if(title && title != "") {
             for(const product of products) {
-                if(product.title.toLowerCase().includes(title)) {
-                    productIds.push(product.id);
+                let str1 = product.title.toLowerCase();
+                let str2 = title;
+                
+                const length = Math.min(str1.length, str2.length);
+                let distance = 0;
+
+                for(let i = 0; i < length; i++) {
+                    distance += Math.abs(str1.charCodeAt(i) - str2.charCodeAt(i));
                 }
+
+                productIds.push({id: product.id, distance: distance});
             }
         }
 
-        return productIds;
-    }
-
-    function getCards() {
-        const productIds = getIdArray();
-        let key = 0;
-        const cards = productIds.map((productId) => {
-            key ++;
-            return(
-                <Card className="home-card" cart={cart} setCart={setCart} testId={productId} key={`${key}-${id}`} products={products}/>
-            )
+        productIds.sort((a, b) => {
+            return a.distance - b.distance;
         })
 
-        return cards;
-    }
+        setClosestProducts(productIds.map((product) => {
+            return product.id;
+        }));
+    }, [products, searchParams]);
 
-    const cards = getCards();
+    let key = 0;
+    const cards = closestProducts.slice(0, showlimit).map((productId) => {
+        key ++;
+        return(
+            <Card className="home-card" cart={cart} setCart={setCart} testId={productId} key={`${key}-${id}`} products={products}/>
+        )
+    })
 
     return (
         <main className={`search-main ${className}`}>
             <SearchBar products={products}/>
             <div className="search-cards">
-                {cards}
+                {cards.length > 0 ? cards : <span className="search-span-no-result">No results found</span>}
             </div>
+            {showlimit < closestProducts.length && <button className="booking-main__show-more-button" onClick={() => { setShowLimit((prev) => prev + CONSTANTS.PAGINATION_LIMIT) }}>Show More</button>}
         </main>
     )
 }
